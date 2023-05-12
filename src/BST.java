7c666a35581d84b4bcfe091cbb81704e6e713e0a
asuramaruq <220610@astanait.edu.kz>
2023-05-12 22:30:32 +0600

    final x_x iwkms

--- src/BST.java
+++ src/BST.java
@@ -1,5 +1,8 @@
-public class BST <K extends Comparable<K>,V> {
-    private Node root;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class BST <K extends Comparable<K>,V> implements Iterable<BST.KeyValuePair> {
+    public Node root;
     private class Node<K,V> {
         private K key;
         private V val;
@@ -10,8 +13,11 @@ public class BST <K extends Comparable<K>,V> {
             this.val = val;
             size=1;
         }
+        public String toString(){
+            return "key is "+key+"and value is "+val;
+        }
     }
-    private Node<K,V> put(Node<K,V> node, K key, V val){
+    private Node put(Node<K,V> node, K key, V val){
         if(node==null){
             return new Node<>(key,val);
         }
@@ -19,9 +25,9 @@ public class BST <K extends Comparable<K>,V> {
         if (cmp == 0) {
             return null;
         } else if (cmp > 0) {
-            node.right = put(node.right, key, val);
-        } else {
             node.left = put(node.left, key, val);
+        } else {
+            node.right = put(node.right, key, val);
         }
         node.size= (node.right!=null ? node.right.size : 0) + (node.left!=null ? node.left.size:0) + 1;
         return node;
@@ -29,15 +35,112 @@ public class BST <K extends Comparable<K>,V> {
     public void put(K key, V val){
         root=put(root,key,val);
     }
-    public V get(K key){
-        
-    }
-    public void delete(K key){
 
+    public V get(K key){
+        Node<K,V> temp=get(key,root);
+        return temp.val;
     }
-    public Iterable<K> iterator(){
-        //in order traversal
+    private Node get(K key, Node<K,V> node){
+        int cmp=node.key.compareTo(key);
+        if(cmp==0){
+            return node;
+        }else if(cmp>0){
+            return get(key,node.left);
+        }else{
+            return get(key,node.right);
+        }
+    }
+
+    public void delete(K key){
+        root=delete(root,key);
+    }
+    private Node delete(Node<K,V> node,K key){
+        if(node==null){
+            return node;
+        }
+        int cmp=node.key.compareTo(key);
+        if(cmp==0){
+            if(node.left==null){
+                return node.right;
+            }
+            else if(node.right==null){
+                return node.left;
+            }
+            Node<K,V> minNode=getMin(node.right);
+            node.key=minNode.key;
+            node.val=minNode.val;
+            node.right=delete(node.right,minNode.key);
+        } else if(cmp>0){
+            node.left=delete(node.left,key);
+        }else{
+            node.right=delete(node.right,key);
+        }
+        node.size=(node.right!=null ? node.right.size : 0) + (node.left!=null ? node.left.size:0) + 1;
+        return node;
+    }
+    private Node getMin(Node node){
+        if(node.left!=null){
+            return getMin(node.left);
+        }
+        return node;
+    }
+
+    public void inOrderTraversal(Node<K,V> node){
+        if(node!=null){
+            inOrderTraversal(node.left);
+            System.out.println(node.toString());
+            inOrderTraversal(node.right);
+        }
     }
     //add size
+    public int size(){
+        return root.size;
+    }
     //access both key and value during iteration
+
+    public Iterator<BST.KeyValuePair> iterator(){
+        return new MyBSTIterator(root);
+    }
+    private class MyBSTIterator implements Iterator<BST.KeyValuePair>{
+        private Stack<KeyValuePair> st;
+        public MyBSTIterator(Node<K,V> root){
+            st=new Stack<>();
+            inOrderTraversal(root);
+        }
+        @Override
+        public boolean hasNext(){
+            return !st.empty();
+        }
+        @Override
+        public KeyValuePair next(){
+            return st.pop();
+        }
+        public void inOrderTraversal(Node<K,V> node){
+            if(node!=null){
+                inOrderTraversal(node.right);
+                st.push(new KeyValuePair(node.key,node.val));
+                inOrderTraversal(node.left);
+            }
+            return;
+        }
+    }
+    public class KeyValuePair{
+        private K key;
+        private V val;
+        public KeyValuePair(K key, V val){
+            this.key=key;
+            this.val=val;
+        }
+        public K getKey(){
+            return key;
+        }
+        public V getVal(){
+            return val;
+        }
+        @Override
+        public String toString(){
+            return "key is '"+key+"' and value is '"+val+"'";
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer,String> tree=new BST<Integer,String>();
+        tree.put(50,"is");
+        tree.put(25,"name");
+        tree.put(75,"Olzhas");
+        tree.put(5,"Hello");
+        tree.put(15,"my");
+        for(var elem : tree){
+            System.out.printf(elem.toString());
+            System.out.println();
+        }
+        System.out.println("-------------");
+        tree.delete(25);
+        for(var elem : tree){
+            System.out.printf(elem.toString());
+            System.out.println();
+        }
+        System.out.println("-------------");
+        System.out.println(tree.get(5).toString());
+        System.out.println("-------------");
+        System.out.println(tree.size());
     }
 }
